1b0f346fcd05ee491fc756cebe44ebcd153d0bc1
NidhiKathiriya123 <nidhi.braincuber1@gmail.com>
2024-03-07 11:17:27 +0530

    initial

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const db = require("./src/model");
+const route = require("./src/route/index");
+//const config = require('./src/config/config');
+
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Congratulation API is Working now!!");
+});
+app.use("/api", route);
+
+db.sequelize.sync({ alter: true });
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
+
+
+
